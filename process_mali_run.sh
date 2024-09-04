279b18eca904d8087f18d69c5ffc23f3452f9d6b
Matthew Hoffman <mhoffman@lanl.gov>
2024-09-04 10:40:32 -0600

    add option to detrend

--- process_mali_run.sh
+++ process_mali_run.sh
@@ -1,44 +1,25 @@
 
 MAPFILE=/lustre/scratch5/mhoffman/SLC_AIS_FOR_TIDES_20240830/MALI_data__processing/mapping_file/mapfile_mali_to_slm.nc
 MAPFILE=/lustre/scratch5/mhoffman/SLC_AIS_FOR_TIDES_20240830/MALI_data__processing/mapping_file/mapfile_mali_to_slm.s2n.nc
-CTRL_PATH=/lustre/scratch5/mhoffman/SLC_AIS_FOR_TIDES_20240830/MALI_data__processing/mali_output/ctrlAE
+CTRL_PATH=../ctrlAE
 
 REMOVE_CONTROL=1
 
 for yr in '2015' '2060' '2100' '2200' '2300'; do
-
-if [ $REMOVE_CONTROL -eq 1 ]; then
-
-echo "get initial ctrl thickness"
-lithk_ctrl_init=$exp_out_path/preprocessed/lithk_${name_base_string}_ctrl_init.nc
-ncks -O -d time,0 $ctrl_lithk_20152100 $lithk_ctrl_init
-ncwa -O -a time $lithk_ctrl_init ${lithk_ctrl_init}_notime
-
-echo "calculate ctrl anomaly over time"
-lithk_ctrl_anom=$exp_out_path/preprocessed/lithk_${name_base_string}_ctrl_anomaly.nc
-ncdiff -O $ctrl_lithk_20152100 ${lithk_ctrl_init}_notime $lithk_ctrl_anom
-ncdiff -O 
-
-echo "get last 86 time levels of projection"
-# some runs have 87 instead of 86
-# some models used nonstandard filenames, so use wildcard to find the correct file
-lithkfile=`ls -1 $exp_in_path/*lithk_*.nc`
-lithk_20152100=$exp_out_path/preprocessed/lithk_${IS}_${INST}_${ISM}_2015-2100.nc
-ncks -O -d time,-86, $lithkfile $lithk_20152100
-
-echo "remove ctrl anomaly from projection"
-lithk_anom_adj=$exp_out_path/preprocessed/lithk_${name_base_string}_anomaly_adjusted.nc
-ncdiff -O $lithk_20152100 $lithk_ctrl_anom $lithk_anom_adj
-
-echo "ensure no negative thickness!"
-lithk_anom_adj_cln=$exp_out_path/preprocessed/lithk_${name_base_string}_anomaly_adjusted_cleaned.nc
-ncap2 -O -s "where(lithk<0.0) lithk=0.0" $lithk_anom_adj $lithk_anom_adj_cln
-
-fi
-
-
    echo $yr
-   ncap2 -O -s "where(thickness*910/1028+bedTopography<0) thickness=0.0" mali_${yr}.nc mali_grd_${yr}.nc
+
+   if [ $REMOVE_CONTROL -eq 1 ]; then
+      # calculate ctrl drift
+      ncdiff -O -v thickness ${CTRL_PATH}/mali_${yr}.nc ${CTRL_PATH}/mali_2015.nc ${CTRL_PATH}/mali_diff_from_2015_${yr}.nc
+      # remove ctrl anomaly from projection
+      ncdiff -O -v thickness mali_${yr}.nc ${CTRL_PATH}/mali_diff_from_2015_${yr}.nc mali_dedrifted_${yr}.nc
+      ncks -A -v bedTopography mali_${yr}.nc mali_dedrifted_${yr}.nc
+      ncap2 -O -s "where(thickness<0.0) thickness=0.0" mali_dedrifted_${yr}.nc mali_dedrifted_cleaned_${yr}.nc
+      ncap2 -O -s "where(thickness*910/1028+bedTopography<0) thickness=0.0" mali_dedrifted_cleaned_${yr}.nc mali_grd_${yr}.nc
+   else
+      ncap2 -O -s "where(thickness*910/1028+bedTopography<0) thickness=0.0" mali_${yr}.nc mali_grd_${yr}.nc
+   fi
+
    ncremap -m $MAPFILE -i mali_grd_${yr}.nc -o mali_grd_remapped_${yr}.nc  --add_fll
    #ncatted -a _FillValue,,o,f,0.0 mali_grd_remapped_${yr}.nc
    #ncatted -a _FillValue,,d,, mali_grd_remapped_${yr}.nc
